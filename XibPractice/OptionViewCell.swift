66a2b2694223ea3e37048091f70f51f791808cd0
vinh <dr.vinhhoang@gmail.com>
2022-01-06 00:29:17 +0700

    add custom view

---
+++ XibPractice/OptionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  OptionViewCell.swift
+//  XibPractice
+//
+//  Created by VINH HOANG on 05/01/2022.
+//
+
+import UIKit
+
+class OptionViewCell: UIView {
+
+   
+    @IBOutlet weak var label: UILabel!
+    
+    
+ 
+  
+    
+    
+    
+}
--- XibPractice/ViewController.swift
+++ XibPractice/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var optionViewCell = OptionViewCell()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
 
+    func configureUI() {
+        optionViewCell = OptionViewCell(frame: .zero)
+        view.addSubview(optionViewCell)
+        optionViewCell.translatesAutoresizingMaskIntoConstraints = false
+        
+        optionViewCell.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        optionViewCell.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        optionViewCell.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        optionViewCell.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+    }
+    
+    
 }
 
